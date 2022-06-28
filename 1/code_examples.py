7770852e02552b5975c15ef5eb42e402d143dec3
Vasiliy566 <vasyaisaev31@gmail.com>
2022-06-29 00:14:33 +0300

    add task

---
+++ 1/code_examples.py
@@ -0,0 +1,28 @@
+# Считывание двух строк
+a = input()
+b = input()
+
+# Преобразование строки в число
+a = int(input())
+b = int(input())
+
+print(a + b)
+
+
+# if-elif-else
+a = int(input())
+b = int(input())
+
+if b == 1:
+    print("A")
+elif b > 0:
+    print("Б")
+else:
+    print("C")
+
+
+
+
+
+
+
