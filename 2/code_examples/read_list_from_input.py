10ebeb9f697c4969dcb3d5aab37458154372a3b0
Vasily <vasyaisaev31@gmail.com>
2022-07-04 23:02:33 +0300

    Update read_list_from_input.py

--- 2/code_examples/read_list_from_input.py
+++ 2/code_examples/read_list_from_input.py
@@ -3,3 +3,11 @@ l = []
 for i in range(len(a)):
     l.append(a[i])
 print(l)
+
+
+# Можем преобразовать в цисла так
+a = input().split()
+l = []
+for i in range(len(a)):
+    l.append(int(a[i]))
+print(l)
