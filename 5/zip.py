0e011d77756da95bb7bcbc8e4eb9eb1ac11bc7e0
Vasiliy566 <vasyaisaev31@gmail.com>
2022-07-12 22:01:36 +0300

    add tasks text

---
+++ 5/zip.py
@@ -0,0 +1,7 @@
+names = {"Иван", "Петр", "Петр"}
+moneys = tuple([1000, 1500, 1000])
+
+print(list(zip(names, moneys)))
+# for a, b, c in zip(names, moneys, pets):
+#     print(f"{a} with dog {b} spent {c} moneys")
+
--- example_test/main.py
+++ example_test/main.py
@@ -1,3 +1,2 @@
 from example_test.geometry_figures.circle import get_square
-
 print(get_square(5))
