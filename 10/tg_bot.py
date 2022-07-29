9e6dd38ed8caad81518893e177168ccf911a6dc9
Vasiliy566 <vasyaisaev31@gmail.com>
2022-07-29 21:59:38 +0300

    add tg bot example

--- 10/tg_bot.py
+++ 10/tg_bot.py
@@ -5,8 +5,24 @@ bot = telebot.TeleBot('5599836785:AAFx9GuAzlGQsfLu8RDw5ezbdYL3akFJzSQ')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    print(message)
-    # bot.send_message(message.chat.id, 'Привет')
+    bot.send_message(message.chat.id, 'Привет, я умею /register')
+
+
+@bot.message_handler(commands=['register'])
+def register(message):
+    bot.send_message(message.chat.id, 'Введи пожалуйста свое имя и фамилию')
+    bot.register_next_step_handler(message, read_name)
+
+
+def read_name(message):
+    name = message.text
+    bot.send_message(message.chat.id, f"Введи пожалуйста свою фамилию")
+    bot.register_next_step_handler(message, read_sname, name)
+
+
+def read_sname(message, name):
+    sname = message.text
+    bot.send_message(message.chat.id, f"Спасибо за регистрацию {name} {sname}")
 
 
 bot.infinity_polling()
--- tg_bot_example/register.py
+++ tg_bot_example/register.py
@@ -1,14 +1,35 @@
+import openpyxl
+
+
 def create_user(telegram_id, name, email):
-    with open("users.txt", mode="w") as f:
-        f.write(f"{telegram_id} | {name} | {email}")
+    book = openpyxl.load_workbook('user.xlsx')
+
+    sheet = book.active
+
+    sheet.append((telegram_id, name, email))
+
+    book.save('user.xlsx')
 
 
 def is_user_exists(telegram_id):
-    with open("users.txt") as f:
-        for line in f.readlines():
-            # f"123 | ivanov | example@mail.ru"
-            saved_tg_id = line.split("|")[0].strip()
-            if saved_tg_id == str(telegram_id):
-                return True
-        return False
+    book = openpyxl.load_workbook('user.xlsx')
 
+    sheet = book.active
+
+    row = 2
+    while True:
+        tg_id = sheet.cell(row=row, column=1).value
+        if tg_id is None:
+            return False
+        if str(tg_id) == str(telegram_id):
+            return True
+
+
+if __name__ == "__main__":
+    book = openpyxl.load_workbook('user.xlsx')
+
+    sheet = book.active
+
+    a1 = sheet['A1']
+
+    print(a1.value)
