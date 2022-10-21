2ef19f03a3b0ae09b1ca4a8fe0d1df33c87cdc46
Vasiliy566 <vasyaisaev31@gmail.com>
2022-10-21 17:53:38 +0600

    update examples

--- 6/files/json_examples.py
+++ 6/files/json_examples.py
@@ -2,5 +2,9 @@ import json
 
 with open('json_data.json') as json_file:
     data = json.load(json_file)
-    print(data['Тест1'])
-# < ---
+    print(data)
+
+
+data2 = {"User": 1, "User2": 2}
+with open('json_data2.json', "w") as json_file:
+    json.dump(data2, json_file)
